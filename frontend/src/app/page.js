174e68113e32296fae2183ee5a6a011ee75c164f
laksh kapoor <lakshiskapoor720@gmail.com>
2026-01-05 14:36:52 +0530

    chore: updated some errors in pay and confirm, still will be updated further

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 
 
 /* API URL CONSTANT (HERE) */
-const APIurl = "http://172.25.9.156:3000";
+const APIurl = "http://172.25.8.58:3000";
 
 export default function Page() {
   const [expenses, setExpenses] = useState([]);
@@ -22,6 +22,7 @@ export default function Page() {
       .catch((err) => console.error(err));
   };
 
+ 
   //runs once page reloads
   useEffect(()=>{
     fetchExpenses();
@@ -75,7 +76,35 @@ const handleEdit = (expense) =>{
   setEditingID(expense._id)
 }
 
+ //user sends i paid 
+ const currentUserId = "user1"
+ const markAsPaid =  async(expenseId) => {
+   await fetch(`${APIurl}/api/expenses/${expenseId}/pay`,{
+     method:"POST",
+     headers:{
+       "Content-Type":"application/json"
+     },
+     body: JSON.stringify({
+       userId:currentUserId
+     })
+   })
+   fetchExpenses()
+ }
 
+ //function for the reciever to confirm
+
+ const confirmPayment = async(expenseId)=>{
+  await fetch(`${APIurl}/api/expenses/${expenseId}/confirm`,{
+    method:"POST",
+    headers:{
+     "Content-Type":"application/json"
+    },
+    body: JSON.stringify({
+      userId:currentUserId
+    })
+  });
+  fetchExpenses()
+ }
   return (
     <main style={{ padding: "20px" }}>
       <h1>Expense Tracker</h1>
@@ -109,12 +138,51 @@ const handleEdit = (expense) =>{
           <button type="submit">
             {editingID ? "Update Expense":"Add Expense"}
             </button>
-      </form>
+            
+            {/*show buttons conditionally of confirmation*/}
 
+        
+      </form>
 
       <hr />
 
       {expenses.length === 0 && <p>No expenses found</p>}
+
+      {expenses.map((expense)=>(
+            <div key = {expense._id} style={{}}>
+              <h3>{expense.title}</h3>
+              
+              {expense.splits.map((split)=>(
+                <div key = {split.userId}>
+                  <span>
+                    user {split.userId} = ₹{split.share} ({split.status})
+                    
+                  </span>
+
+                  <p>
+                    DEBUG: currentUserId = {currentUserId}
+                  </p>
+                  <p>
+                    DEBUG: expense.paidBy = {expense.paidBy}
+                  </p>
+
+                  {/*PAY button*/}
+                  {split.userId===currentUserId && split.status === "UNPAID" && (
+                    <button onClick={()=>markAsPaid(expense._id)}>
+                      PAY
+                    </button>
+                  )}
+
+                  {/*CONFIRM button*/}
+                  {expense.paidBy===currentUserId && split.status === "PAID" && (
+                    <button onClick={()=>confirmPayment(expense._id)}>
+                      CONFIRM
+                    </button>
+                  )}
+                </div>
+              ))}
+            </div>
+          ))}
 
       <ul>
         {expenses.map((expense) => (
@@ -129,8 +197,6 @@ const handleEdit = (expense) =>{
               Delete
             </button>
 
-            
-
             <button 
             style={{margin: "10px"}}
             onClick={()=>handleEdit(expense)}
@@ -139,8 +205,13 @@ const handleEdit = (expense) =>{
             </button>
             
           </li>
+
+          
         ))}
       </ul>
+
+
+
     </main>
   );
 }
